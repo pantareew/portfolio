346257fe7f42342e76f7b502fde4fcf45e7d6f02
pantareew <pantaree1459@gmail.com>
2025-12-18 01:14:31 +1100

    update wandoverlay.tsx

---
+++ app/components/WandOverlay.tsx
@@ -0,0 +1,36 @@
+interface WandOverlayProps {
+  wandPosition: { x: number; y: number } | null;
+  onWandMove: (pos: { x: number; y: number }) => void; //function to set wand position
+  radius?: number; //size of glowy light
+}
+
+export default function WandOverlay({
+  wandPosition,
+  onWandMove,
+  radius = 150,
+}: WandOverlayProps) {
+  //update wand position when mouse moves
+  const handleMouseMove = (e: React.MouseEvent) => {
+    onWandMove({ x: e.clientX, y: e.clientY });
+  };
+  return (
+    <div
+      className="absolute inset-0 z-10 pointer-events-auto" //create overlay and receive mouse movement on overlay
+      onMouseMove={handleMouseMove}
+    >
+      {/*wand image */}
+      {wandPosition && (
+        <img
+          src="/assets/wand.png"
+          alt="wand"
+          className="absolute w-48 lg:w-64 h-auto pointer-events-none z-30"
+          //align tip of wand with cursor
+          style={{
+            left: wandPosition.x - 50,
+            top: wandPosition.y - 20,
+          }}
+        />
+      )}
+    </div>
+  );
+}
